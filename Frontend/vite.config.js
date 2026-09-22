6b71362b358aa7c12db87e728a8f52c245b56394
joshalabski <joshuaalabi2008@gmail.com>
2026-09-22 02:39:39 +0100

    Updated backend URL to render

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy:{
       '/api':{
-        target: 'http://localhost:8080',
+        target: 'https://homelyhubmernproject.onrender.com',
         changeOrigin: true,
         secure: false,
       }
